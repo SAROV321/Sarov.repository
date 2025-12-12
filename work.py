8d911d206279e7ac09ef2989170d2733fff8e3db
Sarov Krishna <SarovKrishna1@gmail.com>
2025-12-12 10:58:16 +0530

    work.py modi

--- work.py
+++ work.py
@@ -1,8 +1,22 @@
 
-
 def add(x,y):
     
     return x+y
 result = add(5, 3)
 print("The sum is:", result)
 
+
+def subtract_numbers(a, b):
+    return a - b
+result = subtract_numbers(10, 4)
+print("The difference is:", result)  
+
+def div(x,y)
+    return x/y
+result = divide_numbers(10, 4)
+print(result)  
+
+def mul(x,y)
+   return x*y
+result = multiply_numbers(10, 4)
+print(result)
